8059ba552651c10b27515a39e1fd5da64f0d79d7
wwp <664282857@qq.com>
2021-12-30 17:27:14 +0800

    feat(完成5-12): <el-tooltip content="Top center" placement="top">

---
+++ src/components/lange-select/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dropdown
+    class="international"
+    trigger="click"
+    @command="handleSetLanguage"
+  >
+    <div>
+      <el-tooltip content="国际化" :effect="effect">
+        <svg-icon icon="language"></svg-icon>
+      </el-tooltip>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item :disabled="language === 'zh'" command="zh"
+          >中文</el-dropdown-item
+        >
+        <el-dropdown-item :disabled="language === 'en'" command="en"
+          >English</el-dropdown-item
+        >
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue'
+import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n'
+import { ElMessage } from 'element-plus'
+defineProps({
+  effect: {
+    type: String,
+    default: 'dark',
+    validator: function (value) {
+      return ['dark', 'light'].indexOf(value) !== -1
+    }
+  }
+})
+
+const store = useStore()
+const language = computed(() => store.getters.language)
+
+const i18n = useI18n()
+const handleSetLanguage = (lang) => {
+  //  切换i18n 的local
+  i18n.locale.value = lang
+  //  修改vuex中的保存的local
+  store.commit('app/setLanguage', lang)
+  //  提示
+  ElMessage.success('更新成功')
+}
+</script>
+
+<style lang="scss" scoped></style>
